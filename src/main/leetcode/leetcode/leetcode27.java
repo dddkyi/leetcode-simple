78ef544c2595b0e85b94efb173dac1a0b5db439e
dddkyi <935951002@qq.com>
2019-01-05 19:51:18 +0800

    leetcode27题

---
+++ src/main/leetcode/leetcode/leetcode27.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class leetcode27 {
+
+    public int removeElement(int[] nums, int val) {
+            int i,z;
+            int j=0;
+            int[] nums1=null;
+            if(nums.length==0){
+                return nums.length;
+            }
+            for (i=0;i<nums.length;i++){
+                if(nums[i]==val){
+                    j+=1;
+                    continue;
+                }
+                nums[i-j]=nums[i];
+            }
+            return nums.length-j;
+        }
+    }
